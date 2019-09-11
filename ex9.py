fef0d100092c8169ddf86b9995d3e32e469b4730
Samuel Belo <samuel.belo@al.infnet.edu.br>
2019-09-11 08:08:01 -0300

    ex9

---
+++ ex9.py
@@ -0,0 +1,28 @@
+"""Usando a biblioteca Pygame, escreva um programa que possui uma função que
+desenha um círculo azul de 100 px de diâmetro no centro da tela. (código e printscreen)"""
+
+import pygame
+
+azul = (0, 0, 255)
+branco = (255, 255, 255)
+width = 0
+sair = False
+
+pygame.init()
+tela = pygame.display.set_mode((640, 480))
+tela.fill(branco)
+terminou = False
+
+pygame.draw.circle(tela, azul, (315,220), 100, width)
+
+cont = 60
+clock = pygame.time.Clock()
+while not terminou:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminou = True
+    pygame.display.update()
+    clock.tick(60)
+    cont = cont + 1
+
+pygame.display.quit()
